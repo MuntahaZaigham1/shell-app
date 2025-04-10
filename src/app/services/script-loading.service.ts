fc3e854dffbe74fd6a5ccb3df4783f1430f7c4a4
MuntahaZaigham1 <muntahazaigham10@gmail.com>
2025-04-10 20:40:03 +0500

    added some fixse

--- src/app/services/script-loading.service.ts
+++ src/app/services/script-loading.service.ts
@@ -13,6 +13,10 @@ export class ScriptLoadingService {
             'https://127.0.0.1:4300/assets/js/fileSaver.js',
         ],
         'uibuilder': [
+            "https://unpkg.com/prettier@2.5.1/standalone.js",
+            "https://unpkg.com/prettier@2.5.1/parser-typescript.js",
+            "https://unpkg.com/prettier@2.5.1/parser-html.js",
+            "https://unpkg.com/prettier@2.5.1/parser-angular.js",
             'https://127.0.0.1:4500/assets/js/jquery.slim.min.js',
             'https://127.0.0.1:4500/assets/js/jquery.min.js',
             'https://127.0.0.1:4500/assets/js/popper.min.js',
@@ -20,7 +24,7 @@ export class ScriptLoadingService {
             'https://127.0.0.1:4500/assets/js/jszip.js',
             'https://127.0.0.1:4500/assets/js/fileSaver.js',
             'https://127.0.0.1:4500/assets/js/myjs.js',
-            'https://127.0.0.1:4500/assets/sass.sync.js'
+            'https://127.0.0.1:4500/assets/sass.sync.js',
         ]
     };
 
@@ -29,19 +33,26 @@ export class ScriptLoadingService {
 
     loadRemoteScripts(remoteName: string) {
         const scripts = this.scriptsMap[remoteName];
-        Object.keys(this.scriptsMap).filter(k => k !== remoteName).forEach(k => this.deleteRemoteScripts(k)); // ✅ Clean up scripts
+        Object.keys(this.scriptsMap)
+            .filter(k => k !== remoteName)
+            .forEach(k => this.deleteRemoteScripts(k)); // ✅ Clean up scripts
+    
         if (scripts) {
             scripts.forEach(src => {
-                const script = document.createElement('script');
-                script.src = src;
-                script.type = 'text/javascript';
-                script.async = false;
-                script.defer = true;
-                script.id = `remote-script-${remoteName}-${btoa(src)}`; // unique ID per script
-                document.body.appendChild(script);
+                const scriptId = `remote-script-${remoteName}-${btoa(src)}`;
+                if (!document.getElementById(scriptId)) {
+                    const script = document.createElement('script');
+                    script.src = src;
+                    script.type = 'text/javascript';
+                    script.async = false;
+                    script.defer = true;
+                    script.id = scriptId;
+                    document.body.appendChild(script);
+                }
             });
         }
     }
+    
 
     deleteRemoteScripts(remoteName: string) {
         const scripts = this.scriptsMap[remoteName];
