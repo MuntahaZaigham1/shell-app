9971689d2910741f9b6a5b7dd479d81b3fa9a9f1
MuntahaZaigham1 <muntahazaigham10@gmail.com>
2025-07-17 15:48:08 +0500

    added changes to wait for base application creation in shell

--- src/app/applications/application-list/application-list.component.ts
+++ src/app/applications/application-list/application-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Application } from '../models/application';
@@ -6,6 +6,8 @@ import { ApplicationService } from '../application.service';
 import { Router } from '@angular/router';
 import { InitializeToolsService } from 'src/app/services/initialize-tools.service';
 import { SharedService } from 'fastcode-shared-service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { Subject, takeUntil } from 'rxjs';
 let isShellListenerRegistered = false;
 
 @Component({
@@ -16,13 +18,36 @@ let isShellListenerRegistered = false;
 export class ApplicationListComponent implements OnInit, OnDestroy {
   applications: Application[] = [];
   loading = false;
+  uploading = false;
   hoveredApp: Application | null = null;
+  private uploadStatus$ = new Subject<boolean>();
+  private destroy$ = new Subject<void>();
+
   @ViewChild('deleteDialog') deleteDialog!: TemplateRef<any>;
 
+  private setupUploadStatusListener(): void {
+    this.uploadStatus$.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(uploading => {
+      this.uploading = uploading;
+      this.cd.detectChanges();
+    });
+  }
+
   ngOnInit(): void {
+
+    this.setupUploadStatusListener();
     this.loadApps();
     if (!isShellListenerRegistered) {
       this.initializeMessageListener();
+      window.addEventListener('message', (event) => {
+        if (event?.data?.command === 'uploadedApplicationToGit') {
+          // this.uploading = false;
+          this.uploadStatus$.next(false);
+          this.loadApps();
+          console.log("msg recv from extension, uploadedApplicationToGit");
+        }
+      })
       isShellListenerRegistered = true;
     }
   }
@@ -32,8 +57,11 @@ export class ApplicationListComponent implements OnInit, OnDestroy {
     private initializeToolsService: InitializeToolsService,
     private sharedService: SharedService,
     private router: Router,
-    public dialog: MatDialog
-  ) { }
+    public dialog: MatDialog,
+    private cd: ChangeDetectorRef
+    // private spinner: MatProgressSpinner
+  ) {
+  }
 
   initializeMessageListener(): void {
     this.sharedService.listenMessage(msg => {
@@ -43,30 +71,33 @@ export class ApplicationListComponent implements OnInit, OnDestroy {
           console.log("app-deleted");
         })
       }
-      else if (msg?.command == "codegen-application-created-base" && msg?.for == "shell" && !isShellListenerRegistered) {
-        const metadata = msg.payload.metadata;
-        const zipBlob = msg.payload.zipData;
-        const application: Application = {
-          name: metadata?.name,
-          codegenProjectId: metadata?.id,
-          githubUrl: metadata.githubRepository
-        }
-        isShellListenerRegistered = true;
-        console.log("createApplication is called", metadata?.name, isShellListenerRegistered);
-        this.appService.createApplication(metadata?.name).subscribe((createdApp: Application) => {
-          this.appService.updateApplication(createdApp?.id, application).subscribe(updatedApp => {
-            window.parent.postMessage({
-              command: 'upload-application-to-git',
-              payload: {
-                application: updatedApp,
-                githubRepository: metadata.githubRepository,
-                zip: zipBlob
-              }
-            }, '*');
-
-            this.loadApps();
+      else if (msg?.command == "codegen-application-created-base" && msg?.for == "shell") {
+        // this.uploading = true;
+        if (!isShellListenerRegistered) {
+          this.uploadStatus$.next(true);
+          const metadata = msg.payload.metadata;
+          const zipBlob = msg.payload.zipData;
+          const application: Application = {
+            name: metadata?.name,
+            codegenProjectId: metadata?.id,
+            githubUrl: metadata.githubRepository
+          }
+          isShellListenerRegistered = true;
+          console.log("createApplication is called", metadata?.name, isShellListenerRegistered);
+          this.appService.createApplication(metadata?.name).subscribe((createdApp: Application) => {
+            this.appService.updateApplication(createdApp?.id, application).subscribe(updatedApp => {
+              window.parent.postMessage({
+                command: 'upload-application-to-git',
+                payload: {
+                  application: updatedApp,
+                  githubRepository: metadata.githubRepository,
+                  zip: zipBlob
+                }
+              }, '*');
+              // this.loadApps();
+            })
           })
-        })
+        }
       }
     })
   }
