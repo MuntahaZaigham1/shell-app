58269feaa6b381aaf56863944c3a41f5fd2b3479
MuntahaZaigham1 <muntahazaigham10@gmail.com>
2025-07-21 19:56:04 +0500

    added changes to intercept after authorization token is fetched

--- src/app/core/jwt-interceptor.ts
+++ src/app/core/jwt-interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { filter, Observable, switchMap, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthenticationService } from './services/authentication.service';
 import { CookieService } from './services/cookie.service';
@@ -12,29 +12,49 @@ export class JwtInterceptor implements HttpInterceptor {
 
     constructor(private authService: AuthenticationService, private cookieService: CookieService) { }
 
-
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
-
-        request = request.clone({ headers: request.headers.set('Accept', 'application/json'), body: request.body });
-        if (request.url.search('https://login.microsoftonline.com') == -1) {
+        const isAuthorizationTokenRequest = request.url.endsWith('/auth/getAuthorizationToken');
+        if (isAuthorizationTokenRequest) {
             let headers: any = {
                 "X-XSRF-TOKEN": this.cookieService.get("XSRF-TOKEN"),
                 Accept: 'application/json'
             }
-            if(this.authService.isVsCodeExtension() && this.authService.idToken){
+            if (this.authService.isVsCodeExtension() && this.authService.idToken) {
                 headers["Authentication"] = this.authService.idToken;
             }
-            if (this.authService.authorizationToken) {
-                headers["Authorization"] = this.authService.authorizationToken;
-            }
-
             request = request.clone({
                 withCredentials: true,
                 setHeaders: headers,
                 body: request.body
             });
+            return next.handle(request);
         }
-        return next.handle(request);
+        // For all other requests, wait for authorization state to be ready
+        return this.authService.authorizationState$.pipe(
+            filter(isAuthorized => isAuthorized === true),
+            take(1),
+            switchMap(() => {
+                request = request.clone({ headers: request.headers.set('Accept', 'application/json'), body: request.body });
+                if (request.url.search('https://login.microsoftonline.com') == -1) {
+                    let headers: any = {
+                        "X-XSRF-TOKEN": this.cookieService.get("XSRF-TOKEN"),
+                        Accept: 'application/json'
+                    }
+                    if (this.authService.isVsCodeExtension() && this.authService.idToken) {
+                        headers["Authentication"] = this.authService.idToken;
+                    }
+                    if (this.authService.authorizationToken) {
+                        headers["Authorization"] = this.authService.authorizationToken;
+                    }
+
+                    request = request.clone({
+                        withCredentials: true,
+                        setHeaders: headers,
+                        body: request.body
+                    });
+                }
+                return next.handle(request);
+            }))
     }
 }
